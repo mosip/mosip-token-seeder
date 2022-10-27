2410465cdca801ca23a22e2304fccc9cc56c8339
Lalith Kota <kotalalith@gmail.com>
2022-10-28 02:36:30 +0530

    [MOSIP-24434] develop : Re ordered mapping service

--- mosip_token_seeder/authtokenapi/service/mapping_service.py
+++ mosip_token_seeder/authtokenapi/service/mapping_service.py
@@ -17,49 +17,46 @@ class MappingService:
         self.mandatory_validation_auth_fields = config.authtoken.mandatory_validation_auth_fields.split(',')
 
     def validate_auth_data(self, authdata : dict, mapping: MapperFields, language):
-
         #"." is used to identify if the target auth_data is having any nested json.
         #for eg. if vid is inside a json object qrcodedata the mapping definition could be qrcodedata.vid
 
         final_dict = {}
-        
-        if not self.nested_json_utils.whether_nested_field_in(mapping.vid, authdata):
-            return None, 'ATS-REQ-009', 'vid or its mapping not present'
+
         authdata_vid = self.nested_json_utils.extract_nested_value(mapping.vid, authdata)
+        if not authdata_vid:
+            return None, 'ATS-REQ-009', 'vid or its mapping not present'
         # if len(authdata_vid) <= 16 and len(authdata_vid) >= 19:
         #     return None, 'ATS-REQ-002', 'invalid vid construct'
         final_dict['vid'] = authdata_vid
 
         name_arr = []
+        name_present = False
         for name_var in mapping.name:
             authdata_name_var = self.nested_json_utils.extract_nested_value(name_var, authdata)
+            if authdata_name_var is not None:
+                name_present = True
             if authdata_name_var:
                 name_arr.append(authdata_name_var)
-        authdata_name = self.config.root.name_delimiter.join(name_arr)
         if 'name' in self.mandatory_validation_auth_fields:
-            if not authdata_name:
+            if not name_present:
                 return None, 'ATS-REQ-010', 'name or its mapping not present'
-            if len(authdata_name) == 0:
+            if len(name_arr) == 0:
                 return None, 'ATS-REQ-003', 'name is not provided'
-        if authdata_name:
-            final_dict['name'] = [{'language':language,'value': authdata_name}]
+        if len(name_arr) > 0:
+            final_dict['name'] = [{'language':language,'value': self.config.root.name_delimiter.join(name_arr)}]
 
         authdata_gender = self.nested_json_utils.extract_nested_value(mapping.gender, authdata)
         if 'gender' in self.mandatory_validation_auth_fields:
-            if not self.nested_json_utils.whether_nested_field_in(mapping.gender, authdata):
+            if authdata_gender is None:
                 return None, 'ATS-REQ-011', 'gender or its mapping not present'
             elif len(authdata_gender) == 0:
                 return None, 'ATS-REQ-004', 'gender is empty'
-            # if len(authdata_gender) > 256:
-            #     return None, 'ATS-REQ-005', 'gender value is wrong'
-            # if authdata_gender.lower() not in ['male','female','others']:
-            #     return None, 'ATS-REQ-005', 'gender value is wrong'
         if authdata_gender:
             final_dict['gender'] = [{'language':language,'value': authdata_gender}]
 
         authdata_dob = self.nested_json_utils.extract_nested_value(mapping.dob, authdata)
         if 'dob' in self.mandatory_validation_auth_fields:
-            if not self.nested_json_utils.whether_nested_field_in(mapping.dob, authdata):
+            if authdata_dob is None:
                 return None, 'ATS-REQ-012', 'dateOfBirth or its mapping not present'
             if len(authdata_dob) == 0:
                 return False, 'ATS-REQ-006', 'date of birth is empty'
@@ -70,34 +67,36 @@ class MappingService:
             # except ValueError:
             #     return None, 'ATS-REQ-007', 'not a valid date format for date of birth'
             final_dict['dob'] = self.parse_dob_in_accepted_format(authdata_dob)
-            
+
         authdata_phone = self.nested_json_utils.extract_nested_value(mapping.phoneNumber, authdata)
         if 'phoneNumber' in self.mandatory_validation_auth_fields:
-            if not self.nested_json_utils.whether_nested_field_in(mapping.phoneNumber, authdata):
+            if authdata_phone is None:
                 return None, 'ATS-REQ-013', 'phoneNumber or its mapping not present'
         if authdata_phone:
             final_dict['phoneNumber'] = authdata_phone
 
         authdata_email = self.nested_json_utils.extract_nested_value(mapping.emailId, authdata)
         if 'emailId' in self.mandatory_validation_auth_fields:
-            if not self.nested_json_utils.whether_nested_field_in(mapping.emailId, authdata):
+            if authdata_email is None:
                 return None, 'ATS-REQ-014', 'emailId or its mapping not present'
         if authdata_email:
             final_dict['emailId'] = authdata_email
 
         addr_arr = []
+        addr_present = False
         for addr in mapping.fullAddress:
             addr_val  = self.nested_json_utils.extract_nested_value(addr, authdata)
+            if addr_val is not None:
+                addr_present = True
             if addr_val:
                 addr_arr.append(addr_val)
-        authdata_addr = self.config.root.full_address_delimiter.join(addr_arr)
         if 'fullAddress' in self.mandatory_validation_auth_fields:
-            if not authdata_addr:
+            if not addr_present:
                 return None, 'ATS-REQ-015', 'fullAddress or its mapping not present'
-            if len(authdata_addr) == 0:
+            if len(addr_arr) == 0:
                 return False, 'ATS-REQ-008', 'address is empty'
-        if authdata_addr:
-            final_dict['fullAddress'] = [{'language':language,'value': authdata_addr}]
+        if len(addr_arr) > 0:
+            final_dict['fullAddress'] = [{'language':language,'value': self.config.root.full_address_delimiter.join(addr_arr)}]
 
         return AuthTokenBaseModel(**final_dict),'', ''
 
--- mosip_token_seeder/tokenseeder/utils/nested_json.py
+++ mosip_token_seeder/tokenseeder/utils/nested_json.py
@@ -12,7 +12,7 @@ class NestedJsonUtils:
         for part in nested_field.split("."):
             part_value = self.get_nested_field_list_item(part_value, part)
             if part_value is None:
-                None
+                return None
         return part_value
 
     def get_nested_field_list_item(self, part_value, part):
@@ -26,33 +26,6 @@ class NestedJsonUtils:
             if (not isinstance(part_value, list)) or arr_index >= len(part_value):
                 return None
             return part_value[arr_index]
-        if part not in part_value:
+        if not (isinstance(part_value, dict) and part in part_value):
             return None
         return part_value[part]
-
-    def whether_nested_field_in(self, nested_field, data : dict):
-        if nested_field in data:
-            return True
-        part_value = data
-        for part in nested_field.split("."):
-            if not self.whether_nested_field_list_item(part_value, part):
-                return False
-            part_value = self.get_nested_field_list_item(part_value, part)
-        return True
-
-    def whether_nested_field_list_item(self, part_value, part):
-        if part_value is None:
-            return False
-        regex_res = re.match(r'(.*)\[(\d+)\]', part)
-        if regex_res:
-            part = regex_res.group(1)
-            arr_index = int(regex_res.group(2))
-            if not self.whether_nested_field_list_item(part_value, part):
-                return False
-            part_value = self.get_nested_field_list_item(part_value, part)
-            if (not isinstance(part_value, list)) or arr_index >= len(part_value):
-                return False
-            return True
-        if part not in part_value:
-            return False
-        return True

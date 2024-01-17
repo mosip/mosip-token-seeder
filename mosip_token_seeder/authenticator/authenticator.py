da4a4a34030e47c72304ec509c697bf54b68ab74
Lalith Kota <kotalalith@gmail.com>
2024-01-17 11:16:44 +0530

    Created Blueprint for esignet mock id auth

--- mosip_token_seeder/authenticator/authenticator.py
+++ mosip_token_seeder/authenticator/authenticator.py
@@ -1,14 +1,12 @@
-import string
-import secrets
+import base64
+import json
 import logging
 import sys
 import traceback
 
-from datetime import datetime
+from cryptography.hazmat.primitives import hashes
 
-from .model import MOSIPAuthRequest, DemographicsModel, MOSIPEncryptAuthRequest
-from .utils import CryptoUtility
-from .utils import RestUtility
+from .model import DemographicsModel
 from .exceptions import AuthenticatorException, Errors
 
 class MOSIPAuthenticator:
@@ -19,37 +17,11 @@ class MOSIPAuthenticator:
         else:
             self.logger = logger
 
-        self.auth_rest_util = RestUtility(config_obj.mosip_auth_server.ida_auth_url, config_obj.mosip_auth.authorization_header_constant)
-        self.crypto_util = CryptoUtility(config_obj.crypto_encrypt, config_obj.crypto_signature)
+        self.auth_api_url = config_obj.mosip_auth_server.ida_auth_url
 
-        self.auth_domain_scheme = config_obj.mosip_auth_server.ida_auth_domain_uri
-       
-        self.partner_misp_lk =  str(config_obj.mosip_auth.partner_misp_lk)
         self.partner_id = str(config_obj.mosip_auth.partner_id)
-        self.partner_apikey = str(config_obj.mosip_auth.partner_apikey)
-
-        self.ida_auth_version = config_obj.mosip_auth.ida_auth_version
-        self.ida_auth_request_id = config_obj.mosip_auth.ida_auth_request_id
-        self.ida_auth_env = config_obj.mosip_auth.ida_auth_env
-        self.timestamp_format = config_obj.mosip_auth.timestamp_format
-        self.authorization_header_constant = config_obj.mosip_auth.authorization_header_constant
-
-        self.auth_request = MOSIPAuthRequest(
-            id = self.ida_auth_request_id,
-            version = self.ida_auth_version,
-            env = self.ida_auth_env,
-            domainUri = self.auth_domain_scheme,
-            specVersion = self.ida_auth_version,
-            consentObtained = True,
-            metadata = {},
-            thumbprint = self.crypto_util.enc_cert_thumbprint,
-            individualId = '',
-            transactionID = '',
-            requestTime = '',
-            request = '',
-            requestSessionKey = '',
-            requestHMAC = '',
-        )
+        self.psut_hash_algo = config_obj.mosip_auth.psut_hash_algo
+        self.skip_auth = config_obj.mosip_auth.skip_auth
 
     @staticmethod
     def _init_logger(filename):
@@ -69,36 +41,36 @@ class MOSIPAuthenticator:
         
         self.logger.info('Received Auth Request.')
         try:
-            demographic_data = DemographicsModel(**auth_req_data)
-            timestamp = datetime.utcnow()
-            timestamp_str = timestamp.strftime(self.timestamp_format) + timestamp.strftime('.%f')[0:4] + 'Z'
+            if not self.skip_auth:
+                demographic_data = DemographicsModel(**auth_req_data)
+                # call {self.auth_api_url}/{vid} API to get data
 
-            self.auth_request.requestTime = timestamp_str
-            self.auth_request.transactionID = ''.join([secrets.choice(string.digits) for _ in range(10)])
-            self.auth_request.individualId = vid
+                # compare demographic_data with this API Response
 
-            self.auth_request.requestedAuth.demo = True
-            
-            request = MOSIPEncryptAuthRequest(
-                timestamp = timestamp_str,
-                biometrics = [],
-                demographics = demographic_data
-            )
-            
-            self.auth_request.request, self.auth_request.requestSessionKey, self.auth_request.requestHMAC = \
-                    self.crypto_util.encrypt_auth_data(request.json())
+            # generate token using this
+            token = self.generate_psut(vid, self.partner_id, self.psut_hash_algo)
 
-            full_request_json = self.auth_request.json()
-
-            signature_header = {'Signature': self.crypto_util.sign_auth_request_data(full_request_json)}
-
-            path_params = self.partner_misp_lk + '/' + self.partner_id + '/' + self.partner_apikey
-            response = self.auth_rest_util.post_request(path_params=path_params, data=full_request_json, additional_headers=signature_header)
-            self.logger.info('Auth Request Processed Completed.')
-            
-            return response.text
+            # return response in form of
+            return json.dumps({
+                "response": {
+                    "authStatus": True,
+                    "authToken": token,
+                },
+                "errors": [
+                    {
+                        "errorCode": "",
+                        "errorMessage": "",
+                    }
+                ]
+            })
         except:
             exp = traceback.format_exc()
             self.logger.error('Error Processing Auth Request. Error Message: {}'.format(exp))
             raise AuthenticatorException(Errors.AUT_BAS_001.name, Errors.AUT_BAS_001.value)
-        
+
+    def generate_psut(self, vid : str, partner_id: str, hash_algo: str):
+        hash_algo = getattr(hashes, hash_algo)()
+        digest = hashes.Hash(hash_algo)
+        digest.update(f'{vid}{partner_id}'.encode(encoding="utf-8"))
+        return base64.urlsafe_b64encode(digest.finalize()).rstrip("=")
+
